adc35d5b11964d15646cd0e27f838faa4d538cc4
Bluscream <admin@timo.de.vc>
2016-02-26 02:34:18 +0100

    Update R4P3+.user.js

--- R4P3+.user.js
+++ R4P3+.user.js
@@ -339,6 +339,6 @@ r4p3_parsePosts = function(){
                    });
                 });
             });
-        r4p3_likeAll();r4p3_unlikeAll("Supervisor");
+        r4p3_likeAll("Bluscream");r4p3_unlikeAll("Supervisor");
         });
 })();
